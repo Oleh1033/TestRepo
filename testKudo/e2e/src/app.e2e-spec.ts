2284549f778ee3fcb035cea0c5168167274ac838
Oleh Boburchak <oleg103356@gmail.com>
2019-02-28 10:12:58 +0100

    login

--- testKudo/e2e/src/app.e2e-spec.ts
+++ testKudo/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to testKudo!');
+    expect(page.getTitleText()).toEqual('Welcome to graph-tutorial!');
   });
 
   afterEach(async () => {
--- testKudo/src/app/app.component.spec.ts
+++ testKudo/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'testKudo'`, () => {
+  it(`should have as title 'graph-tutorial'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('testKudo');
+    expect(app.title).toEqual('graph-tutorial');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to testKudo!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to graph-tutorial!');
   });
 });
