2faed31aaaa65a9a369bda37f774538edeb417f4
Oleh Boburchak <oleg103356@gmail.com>
2019-03-04 15:34:53 +0100

    firstcommit

--- testKudo/src/oauth.ts
+++ testKudo/src/oauth.ts
@@ -1,5 +1,5 @@
 export const OAuthSettings = {
-    appId: '0b0f5f74-3088-4495-bb3d-dcc6966fd7d2',
+    appId: 'f46a92e5-ba4a-49ab-ab6b-9add27027c60',
     scopes: [
       "user.read",
       "calendars.read",
